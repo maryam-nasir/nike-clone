c8c9a423b6e77a7e6d3014144bcad9407b0ce50a
Maryam Nasir <maryamnasir555@yahoo.com>
2023-12-13 18:00:34 +0500

    Add navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Hero, PopularProducts, SuperQuality, Services, SpecialOffer, CustomerReviews, Subscribe, Footer } from "./sections";
+import Nav from "./components/Nav";
 
 const App = () => (
   <main className="relative">
-    Nav
+    <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b"><Hero /></section>
     <section className="padding"><PopularProducts /></section>
     <section className="padding"><SuperQuality /></section>

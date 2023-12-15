b0b664be9f434bc632c1e04d3356e039dc9d706c
Maryam Nasir <maryamnasir555@yahoo.com>
2023-12-15 13:13:36 +0500

    Add Subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
-const Button = ({ label, iconURL, iconAltText, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconURL, iconAltText, backgroundColor, borderColor, textColor, fullWidth }) => {
     return (
         <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full 
-        ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : `bg-coral-red  text-white border-coral-red`}`}>
+        ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : `bg-coral-red  text-white border-coral-red`} ${fullWidth &&
+            'w-full'}`}>
             {label}
             {iconURL && <img src={iconURL} alt={iconAltText} className="ml-2 rounded-full w-5 h-5" />}
 

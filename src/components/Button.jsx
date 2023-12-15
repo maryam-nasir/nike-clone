c332f1ce8b39fb6612ed8590869cbae201555cf4
Maryam Nasir <maryamnasir555@yahoo.com>
2023-12-15 11:24:35 +0500

    Add Special offer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
-const Button = ({ label, iconURL, iconAltText, }) => {
+const Button = ({ label, iconURL, iconAltText, backgroundColor, borderColor, textColor }) => {
     return (
-        <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+        <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full 
+        ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : `bg-coral-red  text-white border-coral-red`}`}>
             {label}
             {iconURL && <img src={iconURL} alt={iconAltText} className="ml-2 rounded-full w-5 h-5" />}
 
